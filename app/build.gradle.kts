4fe7989a6b71c0baf9f53f1a10c5fd1c33b1c48d
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2023-12-06 01:15:33 +0800

    upgrading some dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
